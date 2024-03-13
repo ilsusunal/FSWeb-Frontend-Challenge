32a6b0d29d831bc1d7e6f98858835bf1c0c2a8ad
ilsu-sunal <sunalilsu@gmail.com>
2024-03-13 23:32:46 +0300

    proje imajları düzeltildi

--- src/contexts/LangChangeContext.jsx
+++ src/contexts/LangChangeContext.jsx
@@ -9,36 +9,40 @@ const LangProvider = ({ children }) => {
     const [language, setLanguage] = useLocalStorage('language', 'en');
     const [translations, setTranslations] = useState({});
     const [loading, setLoading] = useState(true); // Sayfanın yüklenme durumu
+    const [isFirstLoad, setIsFirstLoad] = useState(true);
 
     // Dil dosyasını yükle
-    useEffect(() => {
-        const loadTranslations = async () => {
-            try {
-                const response = await axios.get('https://65f1e7c6034bdbecc763ff98.mockapi.io/api/v1/languages',{
-                    headers: {'content-type': 'application/json'}
-                });
-                const filteredData = response.data.find(item => item.id === language); // Seçilen dile göre filtreleme
-                setTranslations(filteredData || {}); // Eğer dil bulunamazsa boş obje atar
-                setLoading(false); // Yükleme tamamlandı
-                toast('Merhaba! Hoşgeldin!');
-            } catch (error) {
-                console.error('Error fetching translations:', error);
+    const loadTranslations = async () => {
+        try {
+            const response = await axios.get('https://65f1e7c6034bdbecc763ff98.mockapi.io/api/v1/languages',{
+                headers: {'content-type': 'application/json'}
+            });
+            const filteredData = response.data.find(item => item.id === language); // Seçilen dile göre filtreleme
+            setTranslations(filteredData || {}); // Eğer dil bulunamazsa boş obje atar
+            setLoading(false); // Yükleme tamamlandı
+            if (isFirstLoad) {
+                toast('Merhaba!');
+                setIsFirstLoad(false); 
             }
-        };
+        } catch (error) {
+            console.error('Error fetching translations:', error);
+        }
+    };
 
+    useEffect(() => {
         loadTranslations(); // Dil değiştiğinde dil dosyasını yükle
-    }, []);
+    }, [language]);
 
     const changeLanguage = (newLanguage) => {
         setLanguage(newLanguage); // Dili değiştir
     };
 
-    // Yüklenme durumunu kontrol et, eğer yükleme tamamlanmadıysa yüklenmeyi bekle
+    // Yüklenmeyi beklerken bunu render et
     if (loading) {
         return <div>Loading...</div>;
     }
 
-    // Yükleme tamamlandıysa, render et
+    // Yükleme tamamlandıysa, saydayı render et
     return (
         <LangChangeContext.Provider value={{ language, translations, changeLanguage }}>
             {children}
