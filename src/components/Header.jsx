aa7246b8da8ab1d9e5fd7a61eec0a415db3b7053
ilsu-sunal <sunalilsu@gmail.com>
2024-03-14 17:57:24 +0300

    klavye erişilebilirliği arttırıldı

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,9 +11,9 @@ export default function Header (){
     return(
         <>
         <header className="flex justify-end px-10 pb-8 pt-20 md:px-60" >
-            <label className="inline-flex items-center cursor-pointer">
+            <label className="inline-flex items-center cursor-pointer" aria-label={theme === "dark" ? (language === "en" ? "Switch to Light Mode" : "Gündüz moduna geçmek için tıklayın") : (language === "en" ? "Switch to Dark Mode" : "Gece moduna geçmek için tıklayın")}>
                 <input type="checkbox" value="" className="sr-only peer" checked={theme === "dark"} onChange={toggleTheme}/>
-                <div className="relative p-2 w-11 h-6 rounded-full peer dark:bg-pink2 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full  after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-yellow2  after:rounded-full after:h-5 after:w-5 after:transition-all  peer-checked:bg-black"></div>
+                <div  tabIndex="0" className="relative p-2 w-11 h-6 rounded-full peer dark:bg-pink2 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full  after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-yellow2  after:rounded-full after:h-5 after:w-5 after:transition-all  peer-checked:bg-black"></div>
                 <span className="ms-3 text-sm font-semibold text-gri2 dark:text-white">
                 {theme === "dark" && language === "tr" ? "GÜNDÜZ" :
                 theme === "dark" && language === "en" ? "LIGHT MODE" :
--- src/components/LanguageToggle.jsx
+++ src/components/LanguageToggle.jsx
@@ -1,10 +1,8 @@
 import React, { useContext } from 'react';
 import { LangChangeContext } from '../contexts/LangChangeContext';
-import { useTheme } from "../contexts/DarkModeContext";
 
 const LanguageToggle = () => {
   const { language, changeLanguage } = useContext(LangChangeContext);
-  const { theme, toggleTheme } = useTheme();
 
   const handleLanguageToggle = () => {
     if (language === 'en') {
@@ -15,7 +13,7 @@ const LanguageToggle = () => {
   };
 
   return (
-    <button onClick={handleLanguageToggle} >
+    <button onClick={handleLanguageToggle} tabIndex="0" aria-label={language === 'en' ? "Türkçe'ye geçmek için tıklayın" : 'Click to switch to English'}>
       {language === 'en' ? 
         <span className='text-sm font-semibold text-gri2 dark:text-white'><span className="first:text-pink2">TÜRKÇE</span> 'YE GEÇ</span> : 
           <span className='text-sm font-semibold text-gri2 dark:text-white'>SWITCH TO<span className="text-pink2"> ENGLISH</span></span>}
